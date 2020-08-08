f74497a629ed234231b7b636dde03bf320c211b5
Vitor Pontual <vitorpguedes@me.com>
2020-08-08 11:34:16 -0300

    Atualização das folders e add Desafio 4

---
+++ Desafio/desafio-modulo4/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes')
+const methodOverride = require('method-override')
+
+const server = express()
+
+server.use(express.urlencoded({extended: true}))
+server.use(express.static('public'))
+server.use(methodOverride('_method')) // antes do middleware routes (obrigatório)
+server.use(routes)
+
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', {
+    express: server,
+    autoescape: false,
+    noChace: true
+})
+
+server.listen(5000, () => console.log('server is running...'))
---
+++ Desafio/desafio-modulo4/teachers.js
@@ -0,0 +1,123 @@
+const fs = require('fs')
+const data = require('./data.json')
+const {age, date} = require('./utils')
+
+// show
+exports.show = function(req, res) {
+    const {id} = req.params
+
+    const foundTeacher = data.teachers.find(function(teacher){
+        return teacher.id == id
+    })
+
+    if (!foundTeacher) return res.send('Teacher not found...')
+
+    const teacher = {
+        ...foundTeacher,
+        age: age(foundTeacher.birth),
+        services: foundTeacher.services.split(', '),
+        created_at: new Intl.DateTimeFormat('pt-BR').format(foundTeacher.created_at)
+    }
+
+    return res.render('teachers/show', {teacher})
+}
+// create
+
+exports.post = function(req, res) {
+    const keys = Object.keys(req.body)
+
+    for (key of keys){
+        if (req.body[key] == '') {
+            return res.send('Please, fill all fields')
+        }}
+
+    let {avatar_url, name, birth, grau, aula, services} = req.body
+
+    birth = Date.parse(birth)
+    const created_at = Date.now()
+    const id = Number(data.teachers.length + 1)
+
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        grau,
+        aula,
+        services,
+        created_at
+    })
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), err => {
+        if(err){
+            console.log(err)
+        }
+        return res.redirect('/teachers')
+    })
+}
+
+// edit
+
+exports.edit = function(req, res) {
+    const {id} = req.params
+
+    const foundTeacher = data.teachers.find(function(teacher){
+        return id == teacher.id
+    })
+
+    if (!foundTeacher) return res.send('Teacher not found')
+
+    const teacher = {
+        ...foundTeacher,
+        birth: date(foundTeacher.birth)
+    }
+
+    return res.render('teachers/edit', {teacher})
+}
+
+// update
+
+exports.put = function(req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex){
+        if( id == teacher.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if(!foundTeacher) return res.send('Teacher not found!')
+
+    const teacher = {
+        ...foundTeacher,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.teachers[index] = teacher
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send('Write file error!')
+
+        return res.redirect(`/teachers/${id}`)
+    })
+
+}
+
+// delete 
+
+exports.delete = function(req, res) {
+    const { id } = req.body
+    const filteredTeachers = data.teachers.filter(function(teacher) {
+        return teacher.id != id
+    })
+
+    data.teachers = filteredTeachers
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(err){
+        if(err) return res.send('Write file error!')
+    })
+
+    return res.redirect('/teachers')
+}
