fe438352d53d8da1c3f55b5ff3744f2faf2b2bb2
Vitor Pontual <vitorpguedes@me.com>
2020-08-18 14:39:53 -0300

    Criação da tabela e Estilização
    Criação da tabela com width de 100% do card
    Componentes Thead com tr + th (HEAD CELL)
    Componentes Tbody com o tr + td (DATA CELL)
    
    Estilização criação da class table-container

--- Desafio/desafio-modulo4/teachers.js
+++ Desafio/desafio-modulo4/teachers.js
@@ -2,6 +2,11 @@ const fs = require('fs')
 const data = require('./data.json')
 const {age, date} = require('./utils')
 
+
+// index
+exports.index = (req, res) => {
+   return res.render('teachers/index', { teachers : data.teachers })
+}
 // show
 exports.show = function(req, res) {
     const {id} = req.params
